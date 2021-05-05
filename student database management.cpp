e2594b0b25c8bcd8c1269e8997da8437a8ae26df
rajat <60007226+krajat0110@users.noreply.github.com>
2021-05-05 07:00:30 +0530

    Update student database management.cpp

--- student database management.cpp
+++ student database management.cpp
@@ -1,173 +1,293 @@
 #include <iostream>
-#include <cstdio>
-#include <cstring>
-#include <cstdlib>
-#include <conio.h>
-#include <iomanip>
+#include<fstream>
+#include<iomanip>
 
 using namespace std;
-int main()
+
+class student
 {
-    FILE *fp, *ft;
-    char another, choice;
 
-    struct student
+  int idnum;
+  char name[50];
+  int cpp, cloud_computing, drone ,hpc,evs;
+  double per;
+  char grade;
+
+public:
+   void calculate();
+   void getdata();
+   void showdata() const;
+   void show_tabular() const;
+   int getIDNum() const;
+};
+
+void Student:: calculate()
+{
+  per=(cpp+cloud_computing+drone+hpc+evs)/5.0;
+  if(per>=90)
+  grade='A+';
+  else if (per>=80)
+  grade='A';
+  else if (per>=75)
+  grade='A-';
+  else if (per>=70)
+  grade='B+';
+  else if(per>=65)
+  grade='B';
+  else if (per>=60)
+  grade='B-';
+  else if(per>=55)
+  grade='C+';
+  else if (per>=50)
+  grade='C';
+  else 
+  grade='F';
+}
+void student::getdata()
+{
+cout«"\nEnter The ID number of the student "; 
+cin>>idnum; 
+cout<<"\n\nEnter student's Name: ";
+cin.ignore(); 
+cin.getline(name,50);
+cout«"\nEnter student's CPP grade: "; 
+cin»cpp;
+cout<<"\nEnter student's Cloud computing grade: "; 
+cin»cloud_computing; 
+cout«"\nEnter student's Drone grade: ";
+cin»drone;
+cout«"\nEnter student's High Performance Computing grade: ";
+cin»hpc;
+cout«"\nEnter student's Environmental studies grade: "; 
+cin»evs; 
+calculate(); 
+} 
+
+void student showdata() const
+{
+  cout<<"\n ID Number: " <<idnum;
+  cout<<"\nName: "<<name;
+  cout<<"\nCPP: "<<cpp;
+  cout<<"\ncloud_computing: "<<cloud_computing;
+  cout<<"\ndrone: "<<drone;
+  cout<<"\nHigh Performance computing:"<<hpc;
+  cout<<"\nEnvironmental studies: "<<evs;
+  cout<<"\nPercentage: "<<per;
+  cout<<"\nLetter Grade: "<<grade;
+}
+Void student::show_tabular() const
+{
+  cout<<idnum<<setw(6)<<" "<<name<<setw(10)<<cpp<<setw(4)<<cloud_computing<<setw(4)<<drone<<setw(4)<<hpc<<setw(4)<<evs<<setw(8)<<per<<setw(6)<<grade<<endl;
+}
+
+int student::getIDNum() const
+{
+  return idnum;
+}
+
+void SaveStudent();
+void displayAll();
+void Searchdisplay(int);
+void modifyStudent(int);
+void DisplayClassResult();
+void DisplayResult();
+
+void write_student()
+{
+  student st;
+  ofstream outfile;
+  outfile.open("student.dat",ios::binary|ios::app);
+  st.getdate();
+  outFile.write(reinterpret_cast<char *> (&st), sizeof(student));
+  outFile.close();
+  cout<<"\n\nStudent record has been Created ";
+  cin.ignore();
+  cin.get();
+}
+
+void display_all()
+{
+  student stl;
+  ifstream inFiles;
+  inFile.open("student.dat",ios::binary);
+  if(!inFile)
+  {
+    cout<<"File could not be open !! press any key....";
+    cin.ignore();
+    cin.get();
+    return;
+  }
+  cout<<"\n\n\n\tDISPLAY ALL RECORD !!\n\n"
+  while(inFile.read(reintrperet_cast<char *>(&st),sizeof(student)))
+  {
+    st.showdata();
+    cout<<"\n\n============================\n";
+  }
+    inFile.close();
+    cin.ignore();
+    cin.get();
+}
+
+  void display_sp(int n)
+  {
+    student st;
+    ifstream inFile;
+    inFile.open("student.dat",ios::binary);
+    if(!inFile)
     {
-        char first_name[50], last_name[50];
-        char course[100];
-        int section;
-    };
-
-    struct student e;
-    char xfirst_name[50], xlast_name[50];
-    long int recsize;
-
-    fp=fopen("users.txt","rb+");
-
-    if (fp == NULL)
-    {
-        fp = fopen("users.txt","wb+");
-
-        if (fp==NULL)
-        {
-            puts("Cannot open file");
-            return 0;
-        }
+      cout<<"File could not be open !! press any key...";
+      cin.ignore;
+      cin.get();
+      return;
     }
 
-
-    recsize = sizeof(e);
-
-    while(1)
+    bool flag=false;
+    while(inFile,read(reinterpret_cast<char*>(&st),sizeof(student)))
     {
-        system("cls");
-
-        cout << "\t\t====== STUDENT DATABASE MANAGEMENT SYSTEM ======";
-        cout <<"\n\n                                          ";
-        cout << "\n\n";
-        cout << "\n \t\t\t 1. Add    Records";
-        cout << "\n \t\t\t 2. List   Records";
-        cout << "\n \t\t\t 3. Modify Records";
-        cout << "\n \t\t\t 4. Delete Records";
-        cout << "\n \t\t\t 5. Exit   Program";
-        cout << "\n\n";
-        cout << "\t\t\t Select Your Choice :=> ";
-        fflush(stdin);
-        choice = getche();
-        switch(choice)
-        {
-        case '1' :
-            fseek(fp,0,SEEK_END);
-            another ='Y';
-            while(another == 'Y' || another == 'y')
-            {
-                system("cls");
-                cout << "Enter the Firt Name : ";
-                cin >> e.first_name;
-                cout << "Enter the Last Name : ";
-                cin >> e.last_name;
-                cout << "Enter the Course    : ";
-                cin >> e.course;
-                cout << "Enter the Section   : ";
-                cin >> e.section;
-                fwrite(&e,recsize,1,fp);
-                cout << "\n Add Another Record (Y/N) ";
-                fflush(stdin);
-                another = getchar();
-            }
-            break;
-        case '2':
-            system("cls");
-            rewind(fp);
-            cout << "=== View the Records in the Database ===";
-            cout << "\n";
-            while (fread(&e,recsize,1,fp) == 1)
-            {
-                cout << "\n";
-                cout <<"\n" << e.first_name << setw(10)  << e.last_name;
-                cout << "\n";
-                cout <<"\n" <<e.course <<  setw(8)  << e.section;
-            }
-            cout << "\n\n";
-            system("pause");
-            break;
-
-        case '3' :
-            system("cls");
-            another = 'Y';
-            while (another == 'Y'|| another == 'y')
-            {
-                cout << "\n Enter the last name of the student : ";
-                cin >> xlast_name;
-
-                rewind(fp);
-                while (fread(&e,recsize,1,fp) == 1)
-                {
-                    if (strcmp(e.last_name,xlast_name) == 0)
-                    {
-                        cout << "Enter new the Firt Name : ";
-                        cin >> e.first_name;
-                        cout << "Enter new the Last Name : ";
-                        cin >> e.last_name;
-                        cout << "Enter new the Course    : ";
-                        cin >> e.course;
-                        cout << "Enter new the Section   : ";
-                        cin >> e.section;
-                        fseek(fp, - recsize, SEEK_CUR);
-                        fwrite(&e,recsize,1,fp);
-                        break;
-                    }
-                    else
-                        cout<<"record not found";
-                }
-                cout << "\n Modify Another Record (Y/N) ";
-                fflush(stdin);
-                another = getchar();
-            }
-            break;
-
-
-        case '4':
-            system("cls");
-            another = 'Y';
-            while (another == 'Y'|| another == 'y')
-            {
-                cout << "\n Enter the last name of the student to delete : ";
-                cin >> xlast_name;
-
-                ft = fopen("temp.dat", "wb");
-
-                rewind(fp);
-                while (fread (&e, recsize,1,fp) == 1)
-
-                    if (strcmp(e.last_name,xlast_name) != 0)
-                    {
-                        fwrite(&e,recsize,1,ft);
-                    }
-                fclose(fp);
-                fclose(ft);
-                remove("users.txt");
-                rename("temp.dat","users.txt");
-
-                fp=fopen("users.txt","rb+");
-
-                cout << "\n Delete Another Record (Y/N) ";
-                fflush(stdin);
-                another = getchar();
-            }
-
-            break;
-
-        case '5':
-            fclose(fp);
-            cout << "\n\n";
-            cout << "\t\t     THANK YOU FOR USING THIS SOFTWARE";
-            cout << "\n\n";
-            exit(0);
-        }
+      if(st,getIDNum()==n){
+        dt.showdata();
+        flag=true;
+      }
     }
+    infile.close();
+    if(flag==false)
+    cout<<"\n\nrecord not exist";
+    cin.ignore();
+    cin.get();
+  }
+
+void modify_student(int n)
+{
+  bool found=false;
+  student st;
+  fstream File;
+  File.opem("student.dat",ios::binary|ios::in|ios::out);
+  if(!file)
+  {
+    cout<<"File could not be open !! press any key...";
+    cin.ignore();
+    cin.get();
+    return;
+  }
+  while(!file.eof() && found==false)
+  {
+    File.read(reinterpret_cast<char*>(&st), sizeof(student));
+    if(st,getIDNum()==n)
+    {
+      st.showdata();
+      cout<<"\n\nPlease  Enter The New Details of student"<<Endl;
+      st.getdata();
+      int pos=(-1)*static_cast<int>(sizeof(st));
+      File.seek(pos,ios::cur);
+      File.write(reinterpret_cast<char *>(&st),sizeof(student));
+      ccout<<"\n\n\t Record Updated";
+      found=true;
+    }
+  }
+ File.close();
+ if(found==false)
+ cout<<"\n\n Record Not Found ";
+ cin.ignore();
+ cin.get();
+}  
 
 
-    system("pause");
-    return 0;
+void delete_student(int n)
+{
+  student st;
+  ifstream inFile;
+  inFile.open("student.dat",ios::binary);
+  if(!inFile){
+    cout<<"File could not be open !! press any key..."
+    cin.ignore();
+    cin.get();
+    return;
+  }
+  ofstream outFile;
+  outFile.open("Temp.dat",ios::out);
+  inFiel.seekg(0,ios::beg);
+  while(inFile.read(reinterpret_cast<char *> (&st),sizeof(student)))
+  {
+    if(st.getIDNum()!=n)
+    {
+      outfile.write(reinterpret_cast<char *>(&st), sizeof(student));
+    }
+  }
+  outFile.close();
+  inFile.close();
+  remove("student.dat");
+  rename("Temp.dat","student.dat");
+  cout<<"\n\n\tRecord Deleted ..";
+  cin.ignore();
+  cin.get();
+}
+
+void class_result()
+{
+  student st;
+  ifstream inFile;
+  inFile.open("student.dat",ios::binary);
+  if(!inFile)
+  {
+    cout<<"File could not be open !! press any key...";
+    cin.ignore();
+    cin.get();
+    return;
+  }
+  cout<<"\n\n\t\tALL STUDENTS RESULT \n\n";
+  cout<<"=========================================\n";
+  cout<<"R.No    Name      C++ CC   D   HPC  EVS  %age   GRADE"<<endl;
+  cout<<"=========================================\n";
+  while (inFile.read(reinterpret_cast<char *>(&st),sizeof(student)))
+  {
+    st.show_tabular();
+  }
+  cin.ignore();
+  cin.get();
+  infile.close();
+}
+
+int main() 
+{
+ char chl
+ int num;
+ cout.setf(ios::Fixed|ios::showpoint);
+ cout<<setprecosion(2);
+ do
+ {
+   cout<<"\n\n";
+   cout<<"\t\t\tSTUDENT MANAGEMET SYSTEM";
+   cout<<"\n\n\t++++++++++++++++++++++++++++++++++++";
+   cout<<"\n\n\t\t\t1.CREATE STUDENT RECORD";
+   cout<<"\n\n\t\t\t2.DISPLAY ALL STUDENTS RECORDS";
+   cout<<"\n\n\t\t\t3.DISPLAY ALL STUDENT RECORDS";
+   cout<<"\n\n\t\t\t4.MODIFY STUDENT RECORD";
+   cout<<"\n\n\t\t\t5.DELETE SSTUDENT RECORD";
+   cout<<"\n\n\t\t\t6. DISPLAY CLASS RESULT";
+   cout<<"\n\n\t\t\t7. EXIT";
+   cout<<"\n\n\t++++++++++++++++++++++++++++++++++++";
+   cout<<"\n\n\t\t\tplease Enter your choice(1-7): ";
+   cin>>ch;
+
+   switch(ch)
+   {
+     case '1': write_student(); break;
+     case '2':display_all(); break;
+     case '3': cout<<"\n\n\tPlease Enter Student's ID number: ";
+     cin>>num;
+          display_sp(num); break;
+    case '4': cout<<"\n\n\tPlease Enter Student's ID number: ";
+    cin>>num;
+         modify_student(num); break;
+    case '5': cout<<"\n\n\tPlease Enter Student's ID number: ";
+    cin>>num;
+         delete_student(num); break;  
+    case '6' : class_result(); break;
+    case '7' : exit(0);;
+    default:cout<<"\a";        
+
+   }
+ }while(ch!='7');
+
+ return 0;
 }
